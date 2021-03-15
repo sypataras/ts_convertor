fede42eb6c55421d448bf518082726d41fb2b842
sypataras <tarassypa@ukr.net>
2021-03-15 14:54:30 +0800

    add speed class

--- lib/ts_convertor.rb
+++ lib/ts_convertor.rb
@@ -1,5 +1,10 @@
-require "ts_convertor/version"
+# frozen_string_literal: true
+
+require 'ts_convertor/version'
+require 'ts_convertor/speed'
 
 module TsConvertor
   class Error < StandardError; end
+
+  def self.conver; end
 end
---
+++ lib/ts_convertor/speed.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module TsConvertor
+  class Speed
+    SIZE = %w[km_h mile_h].freeze
+
+    def self.convert_speed(val, from, to)
+      check_variables(form, to)
+      val * send(from, to)
+    end
+
+    private
+
+    def check_variables(from, to)
+      raise StandardError, "FROM variable is undefined, valid variables = #{SIZE}" unless SIZE.include?(from)
+      raise StandardError, "TO variable is undefined, valid variables = #{SIZE}" unless SIZE.include?(to)
+    end
+
+    def km_h(to)
+      return 1 if to.eql?('km_h')
+      return 0.62137119 if to.eql?('mile_h')
+    end
+
+    def mile_h(to)
+      return 1 if to.eql?('mile_h')
+      return 1.609344 if to.eql?('km_h')
+    end
+  end
+end
